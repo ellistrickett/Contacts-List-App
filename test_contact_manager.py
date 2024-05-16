269c7a4d9b3b96461c3f05bcf1d2c8acc4b9b332
E T <ellis.trickett@gmail.com>
2024-05-16 19:44:29 +0100

    Refactor Tests

--- test_contact_manager.py
+++ test_contact_manager.py
@@ -7,26 +7,45 @@ from contact_manager import ContactManager
 
 class TestContactManager(unittest.TestCase):
 
+    test_contact_object_darwin = Contact(
+        "a1ffb27c-f1d1-4e60-bf82-0f94f7434e91",
+        "Darwin",
+        "Nunez",
+        "+447519371555",
+        "darwin@darwin.com",
+        "2024-05-16 19:19:11.077222",
+    )
+
+    test_contact_object_mo = Contact(
+        "1fcd6501-6f43-4bcc-b579-1b8785cc3683",
+        "Mo",
+        "Salah",
+        "+12332322774",
+        "mo@mo.com",
+        "2024-05-16 19:19:11.077222",
+    )
+
+    test_contact_darwin = {
+        "id": "a1ffb27c-f1d1-4e60-bf82-0f94f7434e91",
+        "first_name": "Darwin",
+        "last_name": "Nunez",
+        "phone_number": "+447519371555",
+        "email_address": "darwin@darwin.com",
+        "date_time_updated": "2024-05-16 19:19:11.077222",
+    }
+
+    test_contact_mo = {
+        "id": "1fcd6501-6f43-4bcc-b579-1b8785cc3683",
+        "first_name": "Mo",
+        "last_name": "Salah",
+        "phone_number": "+12332322774",
+        "email_address": "mo@mo.com",
+        "date_time_updated": "2024-05-16 19:19:11.077222",
+    }
+
     @mock.patch("contact_manager.json.load")
     def test_initialise_contact_list(self, mock_json_load):
-        mock_json_load.return_value = [
-            {
-                "id": "a1ffb27c-f1d1-4e60-bf82-0f94f7434e91",
-                "first_name": "Darwin",
-                "last_name": "Nunez",
-                "phone_number": "+447519371555",
-                "email_address": "darwin@darwin.com",
-                "date_time_updated": "2024-05-16 19:19:11.077222",
-            },
-            {
-                "id": "1fcd6501-6f43-4bcc-b579-1b8785cc3683",
-                "first_name": "Mo",
-                "last_name": "Salah",
-                "phone_number": "+12332322774",
-                "email_address": "mo@mo.com",
-                "date_time_updated": "2024-05-16 19:19:11.077222",
-            },
-        ]
+        mock_json_load.return_value = [self.test_contact_darwin, self.test_contact_mo]
         contact_manager = ContactManager()
         contact_manager.initialise_contact_list()
 
@@ -55,34 +74,15 @@ class TestContactManager(unittest.TestCase):
 
     @mock.patch("contact_manager.print")
     def test_add_contact(self, mock_print):
-
-        test_contact = {
-            "a1ffb27c-f1d1-4e60-bf82-0f94f7434e91",
-            "Darwin",
-            "Nunez",
-            "+447519371555",
-            "darwin@darwin.com",
-            "2024-05-16 19:19:11.077222",
-        }
-
         contact_manager = ContactManager()
-        contact_manager.add_contact(test_contact)
+        contact_manager.add_contact(self.test_contact_darwin)
 
         argument = "Cannot add contact to contact list as not a Contact Object"
 
         self.assertEqual(len(contact_manager.contact_list), 0)
         mock_print.asset_called_with(argument)
 
-        test_contact_object = Contact(
-            "a1ffb27c-f1d1-4e60-bf82-0f94f7434e91",
-            "Darwin",
-            "Nunez",
-            "+447519371555",
-            "darwin@darwin.com",
-            "2024-05-16 19:19:11.077222",
-        )
-
-        contact_manager.add_contact(test_contact_object)
+        contact_manager.add_contact(self.test_contact_object_darwin)
 
         self.assertEqual(len(contact_manager.contact_list), 1)
         self.assertEqual(
@@ -98,36 +98,20 @@ class TestContactManager(unittest.TestCase):
 
     def test_get_contact_by_id(self):
 
-        test_contact_object = Contact(
-            "a1ffb27c-f1d1-4e60-bf82-0f94f7434e91",
-            "Darwin",
-            "Nunez",
-            "+447519371555",
-            "darwin@darwin.com",
-            "2024-05-16 19:19:11.077222",
-        )
-
         contact_manager = ContactManager()
-        contact_manager.contact_list = [test_contact_object]
+        contact_manager.contact_list = [
+            self.test_contact_object_darwin,
+            self.test_contact_object_mo,
+        ]
 
         self.assertEqual(
             contact_manager.get_contact_by_id("a1ffb27c-f1d1-4e60-bf82-0f94f7434e91"),
-            test_contact_object,
+            self.test_contact_object_darwin,
         )
-        contact_manager.get_contacts()
 
+        contact_manager.contact_list = [self.test_contact_object_mo]
 
-# test_contact_darwin = Contact(
-#     "a1ffb27c-f1d1-4e60-bf82-0f94f7434e91",
-#     "Darwin",
-#     "Nunez",
-#     "+447519371555",
-#     "darwin@darwin.com",
-# )
-# test_contact_mo = Contact(
-#     "1fcd6501-6f43-4bcc-b579-1b8785cc3683",
-#     "Mo",
-#     "Salah",
-#     "+12332322774",
-#     "mo@mo.com",
-# )
+        self.assertEqual(
+            contact_manager.get_contact_by_id("a1ffb27c-f1d1-4e60-bf82-0f94f7434e91"),
+            None,
+        )
